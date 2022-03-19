d0f17a152458743ba4e136ff55e03c20d7a3ab21
Zeph1rr <grianton535@gmail.com>
2022-03-19 22:53:30 +0300

    returned dotenv

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ const bcrypt = require("bcryptjs");
 const {Users} = require("./models/models");
 const path = require("path")
 
-console.log(process.env)
+const env_values = {
+    "PORT": process.env.PORT,
+    "DB_NAME": process.env.DB_NAME,
+    "DB_USER": process.env.DB_USER,
+    "DB_PASSWORD": process.env.DB_PASSWORD,
+    "DB_HOST": process.env.DB_HOST,
+    "DB_PORT": process.env.DB_PORT,
+    "JWT_SECRET": process.env.JWT_SECRET,
+    "ADMIN_PASSWORD": process.env.ADMIN_PASSWORD
+}
 
 const PORT = process.env.PORT || 5000
 
@@ -34,7 +43,11 @@ const start = async () => {
             const hashedPassword = bcrypt.hashSync(process.env.ADMIN_PASSWORD, 8)
             await Users.create({email: "grianton535@gmail.com", password: hashedPassword, role: 1, name: 'Zeph1rr'})
         }
-        app.listen(PORT, () => { console.log(`Server started on port ${PORT}`)})
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`)
+            console.log('Server started with env_values:')
+            console.log(env_values)
+        })
     } catch (e) {
         console.log(`ERROR: ${e}`)
     }
